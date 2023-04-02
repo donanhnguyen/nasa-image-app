65b9b4c57ebd3dc7e90d0fad385f94ae9cd7ecc6
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-04-02 10:38:57 -0500

    more styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,6 +5,10 @@ import Axios from 'axios';
 import GlobalContext from './GlobalContext';
 import './cardflip.css';
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function Home () {
 
     const {currentUserState, setCurrentUserState} = useContext(GlobalContext);
@@ -29,23 +33,60 @@ function Home () {
 
     function displayPlanets () {
         const displayedPlanets = allPlanets.map((planet) => {
+            var countHowManyHotelsInPlanet = 0;
+            for (let i in hotelsState) {
+                let currentHotel = hotelsState[i];
+                if (planet === currentHotel.planet) {
+                    countHowManyHotelsInPlanet += 1;
+                }
+            }
             return (
-                <div key={planet} className="flip-card single-planet-container">
-                <div className="flip-card-inner">
-                    <div className="flip-card-front">
+                <div key={planet} className="single-planet-container">
+                    <h1>{planet}</h1>
                     <img src={require(`../pics/${planet.split(' ').join('')}.jpg`)} 
                         alt={planet} 
                         className="single-planet-image"
                     />
-                    </div>
-                    <div className="flip-card-back">
-                        <h1>{planet}</h1>
-                    </div>
-                </div>
+                    <p>{countHowManyHotelsInPlanet} hotels</p>
                 </div>
             )
         })
         return displayedPlanets;
+    }
+
+    function displayFeaturedHotels () {
+        if (hotelsState) {
+            var firstArray = hotelsState;
+            var firstFeaturedHotel = firstArray[getRandomInt(firstArray.length)]
+            var secondArray = [];
+            for (let i in hotelsState) {
+                let currentHotel = hotelsState[i];
+                if (currentHotel.name !== firstFeaturedHotel.name) {
+                    secondArray.push(currentHotel);
+                }
+            }
+            var secondFeaturedHotel = secondArray[getRandomInt(secondArray.length)];
+            var featuredHotels = [firstFeaturedHotel, secondFeaturedHotel];
+            const displayThem = featuredHotels.map((hotel) => {
+                return (
+                    <div key={hotel.name} className='flip-card single-featured-hotel'>
+                    <div className="flip-card-inner">
+
+                        <div className="flip-card-front">
+                            <p>{hotel.name}</p>
+                            <img className="hotel-pic-in-featured-page" src={require(`../pics/${hotel.name.split(' ').join('')}.jpg`)}></img>
+                        </div>
+                       
+                        <div className="flip-card-back">
+                            <p>{hotel.description}</p>
+                            <p>Located on planet {hotel.planet}.</p>
+                        </div>
+                    </div>
+                    </div>
+                )
+            })
+            return displayThem;
+        }
     }
 
     return (
@@ -56,11 +97,16 @@ function Home () {
             
             <br></br>
 
+            <h1 className='featured-hotels-heading'>Featured hotels:</h1>
+            <div className='featured-hotels-container'>
+                {displayFeaturedHotels()}
+            </div>
+
+            <br></br>
 
             {/* display all hotel planets here */}
             <h1>Browse by planets:</h1>
             <div className='displayed-planets-container'>
-                
                  {displayPlanets()}
             </div>
            
