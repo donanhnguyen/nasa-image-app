803def0e8c793d0c16ba881eafe81aabff9b67e8
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-22 21:38:27 -0500

    first commit

--- api/index.js
+++ api/index.js
@@ -3,13 +3,8 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoute from './routes/auth.js'; 
 import usersRoute from './routes/users.js'; 
-import hotelsRoute from './routes/hotels.js'; 
-import bookingsRoute from './routes/bookings.js';
-import roomsRoute from './routes/rooms.js'
 import cors from 'cors';
-import Hotel from './models/Hotel.js';
-import Room from './models/Room.js';
-import { seededRooms, seededHotels } from './seed.js';
+
 
 const app = express(); 
 dotenv.config();
@@ -30,27 +25,23 @@ mongoose.connection.on('disconnected', () => {
 
 // middlewares
 
-var corsOptions = {
-    origin: "https://galaxystays.onrender.com"
-}
-// uncomment the below code when in development mode, comment it out in production
 // var corsOptions = {
-//     origin: "http://localhost:3000"
+//     origin: "https://galaxystays.onrender.com"
 // }
+// uncomment the below code when in development mode, comment it out in production
+var corsOptions = {
+    origin: "http://localhost:3000"
+}
 
 app.use(express.json());
 app.use(cors(corsOptions));
 
 app.use('/api/auth/', authRoute);
 app.use('/api/users/', usersRoute);
-app.use('/api/hotels/', hotelsRoute);
-app.use('/api/users/', bookingsRoute);
-app.use('/api/hotels/', roomsRoute);
+
 
 const PORT = process.env.PORT || 8800;
 
 app.listen(PORT, () => {
     connect(); 
-    // Hotel.insertMany(seededHotels);
-    // Room.insertMany(seededRooms);
 })
